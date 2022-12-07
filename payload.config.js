3c58ae338c6e12fa0efcf2c5725b7a790431a8b2
Javier Gutiérrez <dukintosh@gmail.com>
2022-12-07 04:55:35 -0600

    netlify deploy url added

--- payload.config.js
+++ payload.config.js
@@ -5,7 +5,7 @@ import Autores from './collections/Autores';
 import Media from './collections/Media';
 
 export default buildConfig({
-  serverURL: 'https://henry.dkndrd.com',
+  serverURL: 'https://639070aaa015e376a855989f--super-palmier-47d6b7.netlify.app/',
   admin: {
     user: Autores.slug,
   },
